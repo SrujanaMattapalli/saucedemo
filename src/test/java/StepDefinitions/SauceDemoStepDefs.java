b2963cec5dd5f27e0b22df2d873088a900aefd65
Srujana Mattapalli <srujana.mattapally@gmail.com>
2024-11-15 15:31:16 +0000

    formating

--- src/test/java/StepDefinitions/SauceDemoStepDefs.java
+++ src/test/java/StepDefinitions/SauceDemoStepDefs.java
@@ -12,54 +12,61 @@ import pages.ProductPage;
 import utils.DriverManager;
 import utils.ConfigReader;
 
-public class SauceDemoStepDefs {
+public class SauceDemoStepDefs
+{
     private WebDriver driver;
     private LoginPage loginPage;
     private ProductPage productPage;
 
     @Before
-    public void setup() {
+    public void setup()
+    {
         driver = DriverManager.getDriver();
         loginPage = new LoginPage(driver);
         productPage = new ProductPage(driver);
     }
 
     @Given("user navigates to SauceDemo login page")
-    public void user_navigates_to_SauceDemo_login_page() {
+    public void user_navigates_to_SauceDemo_login_page()
+    {
         driver.get(ConfigReader.getProperty("url"));
         Assert.assertTrue("login page not displayed", loginPage.isLoginPageDisplayed());
     }
 
     @When("user enters username and password")
-    public void user_enters_username_and_password() {
+    public void user_enters_username_and_password()
+    {
         String username = ConfigReader.getProperty("username");
         String password = ConfigReader.getProperty("password");
         loginPage.loginToApplication(username, password);
-
         Assert.assertTrue("Product page not displayed", productPage.isProductPageDisplayed());
-
     }
 
     @And("user selects the highest priced item")
-    public void user_selects_the_highest_priced_item() {
+    public void user_selects_the_highest_priced_item()
+    {
         productPage.findHighestPricedItem();
     }
 
     @Then("user adds the selected item to the cart")
-    public void user_adds_the_selected_item_to_the_cart() {
+    public void user_adds_the_selected_item_to_the_cart()
+    {
         productPage.addHighestPricedItemToCart();
         Assert.assertTrue("Product not added to the cart", productPage.isAddToCartConfirmationDisplayed());
     }
 
     @And("user closes the browser")
-    public void user_closes_the_browser() {
+    public void user_closes_the_browser()
+    {
         driver.quit();
     }
 
     @After
 
-    public void tearDown() {
-        if (driver != null) {
+    public void tearDown()
+    {
+        if (driver != null)
+        {
             driver.quit();
         }
     }
